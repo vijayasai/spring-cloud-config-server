d964ed3711804050e03e12852a46d337f928e5ec
Vijayasai <vijaykesanupalli@gmail.com>
2021-05-19 10:46:00 -0400

    first commit

---
+++ src/test/java/com/poc/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.poc.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties ="spring.profiles.active=native")
+class SpringCloudConfigServerApplicationTests {
+	/*
+	 * @Test void contextLoads() { }
+	 */
+
+}
